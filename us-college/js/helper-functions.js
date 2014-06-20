8a30486e3c4f72add291537f8540b62ebc1dd618
nymmsuu <nymmsuu@gmail.com>
2014-06-20 10:27:43 +0700

    removed us-college subsite

--- us-college/js/helper-functions.js
+++
@@ -1,8 +0,0 @@
-function toggleSchoolList(listId){
-    var schoolList = document.getElementById(listId);
-    if (schoolList.style.display != "block") {
-        schoolList.style.display = "block";
-    } else {
-        schoolList.style.display = "none";
-    }
-}
